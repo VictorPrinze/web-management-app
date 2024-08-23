58a21ffedd67238b029c2076835c87b83c20eef2
git_VictorPrinze <kariukivictor226@gmail.com>
2024-08-23 14:27:10 +0300

    new

--- web-management-backend-main/myproject/settings.py
+++ web-management-backend-main/myproject/settings.py
@@ -75,6 +75,7 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
             'use_unicode': True,
+            'sql_mode': 'traditional',
         },
     }
 }
