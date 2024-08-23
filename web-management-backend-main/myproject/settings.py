14e14f4f92bd7b3fe231bcbfeae677ea4aa1a1eb
git_VictorPrinze <kariukivictor226@gmail.com>
2024-08-23 15:19:15 +0300

    new

--- web-management-backend-main/myproject/settings.py
+++ web-management-backend-main/myproject/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-joib=yh)l@$fjb4d+1ep-rs##aeo@wvy8wu$w^xd1wr5+e!y@3'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
